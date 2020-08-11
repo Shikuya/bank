c58678ca301ad672db1439c872820f742f86aa42
MyNoteBook <kkcc0_0@hotmail.com>
2020-08-11 17:12:51 +0700

    :v

--- bank/Program.cs
+++ bank/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Dynamic;
 
 namespace bank
@@ -12,10 +14,9 @@ namespace bank
             String name = "";
         Choicemenu:
             Console.Clear();
-            double dataid = 0;
             String Enter = "";
             String topic = "              =[BANK]=       \r\n\r\n";
-            Console.WriteLine(topic + 
+            Console.WriteLine(topic +
                           "Create an account = Create \r\n" +
                           "Deposit money = Deposit \r\n" +
                           "Withdraw money = Withdraw \r\n" +
@@ -25,31 +26,31 @@ namespace bank
             String Choicemenu = Console.ReadLine();
             Console.Clear();
 
-            if(Choicemenu == "Create" || Choicemenu == "create") //ยังแยกคนไม่ได้ :P จำ Line ไม่ได้รอถามจารขอรับ
+            if (Choicemenu == "Create" || Choicemenu == "create")
             {
-                createmenu:
-                Console.Write(topic + 
+            createmenu:
+                Console.Write(topic +
                              "Input Name : ");
                 name = Console.ReadLine();
                 Console.Write("Input Balance : ");
                 balance = Convert.ToDouble(Console.ReadLine());
                 Console.Write("Input ID Number : ");
-                id = Convert.ToDouble(Console.ReadLine());//
+                id = Convert.ToDouble(Console.ReadLine());
                 Console.Clear();
-                if(id < 0 || id > 9999999999999999)
+                if (id < 0 || id > 9999999999999999)
                 {
                     Console.Clear();
                     goto createmenu;
                 }
                 else
                 {
-                    createfinish:
-                    Console.Write(topic + 
+                createfinish:
+                    Console.Write(topic +
                                       "Create Account Finish \r\n\r\n" +
                                       ": Enter to menu :");
                     Enter = Console.ReadLine();
                     Console.Clear();
-                    if(Enter == "")
+                    if (Enter == "")
                     {
                         goto Choicemenu;
                     }
@@ -59,35 +60,90 @@ namespace bank
                     }
                 }
             }
-            else if(Choicemenu == "Deposit" || Choicemenu == "deposit")
+            else if (Choicemenu == "Deposit" || Choicemenu == "deposit")
             {
-                Console.Write("Input id");
-
-            }
-            else if(Choicemenu == "Withdraw" || Choicemenu == "withdraw")
-            {
-
-            }
-            else if(Choicemenu == "Check" || Choicemenu == "check") // ยังเช็ค เป็นยอดของคนๆ ไม่ได้
-            {
-                Console.Write("Input ID : ");
-                dataid = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine($"Name : {name} ID : {id} balance : {balance} \r\n\r\n");
-                Enter:
-                Console.Write("Enter to menu");
-                Enter = Console.ReadLine();
-                if (Enter == "")
+                Console.Write("Input id : ");
+                double idi = Convert.ToDouble(Console.ReadLine());
+                if (idi == id)
                 {
-                    goto Choicemenu;
+                    Console.WriteLine("You Balance : " + balance);
+                    Console.Write("Add Money Input : ");
+                    double add = Convert.ToDouble(Console.ReadLine());
+                    balance = balance + add;
+                Enter:
+                    Console.Clear();
+                    Console.Write($"Add Money Finish u money is {balance} \r\n\r\n" +
+                        "Plese Enter to menu :");
+                    Enter = Console.ReadLine();
+                    if (Enter == "")
+                    {
+                        goto Choicemenu;
+                    }
+                    else
+                    {
+                        goto Enter;
+                    }
                 }
                 else
                 {
-                    goto Enter;
+                    goto Choicemenu;
+                }
+
+            }
+            else if (Choicemenu == "Withdraw" || Choicemenu == "withdraw")
+            {
+                Console.Write("Input id : ");
+                double idi = Convert.ToDouble(Console.ReadLine());
+                if (idi == id)
+                {
+                    Console.WriteLine("You Balance : " + balance);
+                    Console.Write("Withdraw Money Input : ");
+                    double add = Convert.ToDouble(Console.ReadLine());
+                    balance = balance - add;
+                Enter:
+                    Console.Clear();
+                    Console.Write($"Withdraw Money Finish u money is {balance} \r\n\r\n" +
+                        "Plese Enter to menu :");
+                    Enter = Console.ReadLine();
+                    if (Enter == "")
+                    {
+                        goto Choicemenu;
+                    }
+                    else
+                    {
+                        goto Enter;
+                    }
                 }
             }
-            else
+            else if (Choicemenu == "Check" || Choicemenu == "check") // ยังเช็ค เป็นยอดของคนๆ ไม่ได้
             {
-                goto Choicemenu;
+            Menucheck:
+                Console.Write("Input ID : ");
+                double idi = Convert.ToDouble(Console.ReadLine());
+                if (idi == id)
+                {
+                    Console.WriteLine($"Name : {name} ID : {id} balance : {balance} \r\n\r\n");
+                Enter:
+                    Console.Write("Enter to menu");
+                    Enter = Console.ReadLine();
+                    if (Enter == "")
+                    {
+                        goto Choicemenu;
+                    }
+                    else
+                    {
+                        goto Enter;
+                    }
+                }
+                else if (!(idi == id))
+                {
+                    Console.Clear();
+                    goto Menucheck;
+                }
+                else
+                {
+                    goto Choicemenu;
+                }
             }
         }
     }
